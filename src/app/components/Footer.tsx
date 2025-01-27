345f0864cf3535105b12f31be58d4d3bbb004050
BALKNAoppa <baterdene207@gmail.com>
2025-01-27 12:51:24 +0800

    setup updates

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return (
+      <div className="max-w-1440 mx-auto px-4 sm:px-6 lg:px-8 py-4">
+        <p className="text-sm text-gray-500">© 2025 Your Company. All rights reserved.</p>
+      </div>
+    )
+  };
---
+++ src/app/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+      <div className="max-w-1440 mx-auto px-4 sm:px-6 lg:px-8 py-4">
+        <h1 className="text-2xl font-bold">Header Content TEST</h1>
+      </div>
+    )
+  };
