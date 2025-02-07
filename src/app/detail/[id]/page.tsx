3ef3e65505da080a5c4485ce7424a9213ded454a
BALKNAoppa <baterdene207@gmail.com>
2025-02-07 10:48:20 +0800

    API updates

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,15 +1,90 @@
 "use client";
 
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
+import { getMovieDetail } from "@/utils/getMovieDetail";
+import { useEffect, useState } from "react";
+import { Star } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
-export default function DetailsHome() {
-const params = useParams;
-console.log(params);
+export function Page() {
+  type Movie = {
+    adult: boolean;
+    backdrop_path: string;
+    genre_ids: number[];
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+  };
 
-  
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const { id } = useParams();
+  useEffect(() => {
+    const dataFunction = async () => {
+      const movieList = await getMovieDetail(Number(id));
+      console.log("ALL RESPONSES FOR UPCOMING", movieList);
+      setMovies(movieList.results);
+    };
+    dataFunction();
+  }, [id]);
+  const { push } = useRouter();
+
   return (
-    <div className="w-screen">
-      teeeest
+    <div className="max-w-[1440px] mx-auto px-[20px] mt-[52px] font-inter">
+      <div className="space-y-8">
+        <div className="flex items-center justify-between">
+          <h3 className="text-foreground text-2xl font-semibold">Upcoming</h3>
+          <Link className="inline-flex items-center justify-center gap-2 text-sm font-medium text-primary underline-offset-4 hover:underline h-9 px-4 py-2" href="/category/upcoming">
+            See more
+          </Link>
+        </div>
+        <div className="mt-8 px-4 flex flex-wrap gap-5 lg:gap-8">
+          {movies.slice(0, 10).map((movie) => (
+            <div
+              key={movie.id}
+              className="flex flex-wrap rounded-md"
+            >
+              <a
+                className="group w-[157.5px] overflow-hidden rounded-lg bg-secondary space-y-1 lg:w-[230px]"
+              >
+                <div className="overflow-hidden relative w-[157.5px] h-[234px] lg:w-[230px] lg:h-[340px]">
+                  <Image
+                    src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                    alt={movie.title}
+                    layout="fill"
+                    onClick={() => push(`detail/${movie.id}`)}
+                    className="absolute inset-0 box-border p-0 border-none m-auto block w-0 h-0 min-w-full max-w-full min-h-full max-h-full object-cover"
+                  />
+                </div>
+                <div className="p-2">
+                  <div className="flex flex-col items-start p-[8px] self-stretch mt-0">
+                    <div className="flex items-center gap-x-1">
+                      <Star className="fill-[#fde047] w-[16px] h-[16px] stroke-[#fde047]" />
+                      <p className="font-inter text-[14px] font-medium leading-[20px]">
+                        {movie.vote_average.toFixed(1)}
+                        <span className="text-[#71717A] text-[14px] font-medium leading-[20px]">
+                          /10
+                        </span>
+                      </p>
+                    </div>
+                    <p className="h-14 overflow-hidden text-ellipsis line-clamp-2 text-lg text-foreground">
+                      {movie.title}
+                    </p>
+                  </div>
+                </div>
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/utils/getMovieDetail.tsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const TMDB_BASE_URL = "https://api.themoviedb.org/3";
+const TMDB_API_TOKEN = process.env.TMDB_API_TOKEN;
+
+export const getMovieDetail = async (movieId: number) => {
+  console.log("test", getMovieDetail);
+  
+    try {
+    const response = await axios.get(
+      `${TMDB_BASE_URL}//movie/${movieId}?language=en-US`,
+      {
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${TMDB_API_TOKEN}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.log("Axios Error shuu", error);
+    return error;
+  }
+};
